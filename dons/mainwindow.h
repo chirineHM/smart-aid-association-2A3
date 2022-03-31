af6eedae1a113b9e4bc947f4200431c62955edcc
dali-chebili <80214875+mohamedalichebili@users.noreply.github.com>
2022-03-31 15:22:51 +0100

    Update mainwindow.h

--- dons/mainwindow.h
+++ dons/mainwindow.h
@@ -4,6 +4,11 @@
 #include <QMainWindow>
 #include "dons.h"
 
+#include <QMainWindow>
+#include <QTableWidget>
+#include<QtSerialPort/QSerialPort>
+#include <QtSerialPort/QSerialPortInfo>
+
 namespace Ui {
 class MainWindow;
 }
@@ -17,11 +22,37 @@ public:
     ~MainWindow();
 
 private slots:
+
+    void on_add_clicked();
+
+    void on_delete_2_clicked();
+
+    void on_modif_clicked();
+
+    void on_tabWidget_currentChanged(int index);
+
+   // void on_combosupp_activated(const QString &arg1);
+
+   // void on_combomodif_currentIndexChanged(const QString &arg1);
+
+    //void on_combomodif_activated(const QString &arg1);
+
+    void on_tab_modif_activated(const QModelIndex &index);
+
+    void on_comboBox_activated(const QString &arg1);
+
+    void on_pushButton_2_clicked();
+
+   // void on_tabWidget_tabBarClicked(int index);
+
+
     void on_pushButton_clicked();
+
+    void on_pushButton_3_clicked();
 
 private:
     Ui::MainWindow *ui;
-    Dons D;
+dons d;
 };
 
 #endif // MAINWINDOW_H
